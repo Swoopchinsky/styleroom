dc3ee7165587b545100d00ae8a26ac45207cc86f
Swoopchinsky <parkerswoop93@mail.ru>
2025-10-15 19:26:33 +0300

    udacha loo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
+import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Items from "./components/Items";
 
-function App() {
-  return (
-    <div className="osnova">
-      <Header />
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'panther',
+          img: 'panther.png',
+          desc: 'Масло, холст',
+          category: 'oil',
+          price: '500 000'
+        },
+        {
+          id: 2,
+          title: 'lef',
+          img: 'lef.png',
+          desc: 'Масло, холст',
+          category: 'oil',
+          price: '1 000 000'
+        },
+        {
+          id: 3,
+          title: 'tigr',
+          img: 'tigr.png',
+          desc: 'Масло, холст',
+          category: 'oil',
+          price: '500 000'
+        },
+        {
+          id: 4,
+          title: 'luch',
+          img: 'luch.png',
+          desc: 'Масло, холст',
+          category: 'oil',
+          price: '700 000'
+        },
+        {
+          id: 5,
+          title: 'ring',
+          img: 'ring.png',
+          desc: 'не ржавеющая сталь + янтарь',
+          category: 'style',
+          price: '1 000'
+        }
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className="osnova">
+        <Header />
+        <Items items={this.state.items} />
+        <Footer />
+      </div>
+    )
+  }
 }
 
 export default App;

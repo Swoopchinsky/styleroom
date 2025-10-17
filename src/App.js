09e6b83faea03d1dc5081cd0f8a480e937f50118
Swoopchinsky <parkerswoop93@mail.ru>
2025-10-17 12:24:50 +0300

    udacha loo v.1.1

--- src/App.js
+++ src/App.js
@@ -47,6 +47,30 @@ class App extends React.Component {
           desc: 'не ржавеющая сталь + янтарь',
           category: 'style',
           price: '1 000'
+        },
+        {
+          id: 6,
+          title: 'bullet ring',
+          img: 'ringbullet.png',
+          desc: 'не ржавеющая сталь + пуля 1920 г.',
+          category: 'style',
+          price: '50 000'
+        },
+        {
+          id: 7,
+          title: 'horse',
+          img: 'horse.png',
+          desc: 'масло + холст',
+          category: 'oil',
+          price: '8 000'
+        },
+        {
+          id: 8,
+          title: 'path',
+          img: 'path.png',
+          desc: 'бумага + акварель',
+          category: 'shik',
+          price: '17 000'
         }
       ]
     }

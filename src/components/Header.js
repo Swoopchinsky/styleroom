5f159c80249b1b3fd2979dd787869cc3ca6d3ca5
Swoopchinsky <parkerswoop93@mail.ru>
2025-10-20 19:18:55 +0300

    udacha loo v.1.8

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TiShoppingCart } from "react-icons/ti";
+import Order from './Order';
 
-export default function Header() {
+const showOrders = (props) => {
+    return (
+        <div>
+            {props.orders.map(el => (
+                <Order key={el.id} item={el} />
+            ))}
+        </div>
+    )
+}
+
+const showNothing = () => {
+    return (
+        <div className='empy'>
+            <h2>Добавь товар</h2>
+        </div>
+    )
+}
+
+export default function Header(props) {
+    let [cartOpen, setCartOpen] = useState(false)
+
     return (
         <header>
             <div>
@@ -10,6 +32,15 @@ export default function Header() {
                     <li>Слушай</li>
                     <li>Существую</li>
                 </ul>
+
+                <TiShoppingCart onClick={() => setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`} />
+                {cartOpen && (
+                    <div className='shop-cart'>
+                        {props.orders.length > 0 ?
+                            showOrders(props) : showNothing()}
+
+                    </div>
+                )}
             </div>
             <div className='presentation'></div>
         </header>
